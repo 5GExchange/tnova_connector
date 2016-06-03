d76098ef3e2e72809f713f486cf3f31c832f6eaa
Janos Czentye <czentye@tmit.bme.hu>
2016-06-03 07:36:57 +0100

    Improve connector behavior Move file names into constants Add service initiation skeleton Add NSD conversion

--- connector.py
+++ connector.py
@@ -12,30 +12,60 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import httplib
 import json
+import logging
 import os
 
-from flask import Flask, request, abort
+import requests
+from flask import Flask, Response, request, abort
+
+from converter import TNOVAConverter
+
+PWD = os.path.realpath(os.path.dirname(__file__))
+CATALOGUE_DIR = "vnf_catalogue"
+NSD_DIR = "nsds"
+SERVICE_NFFG_DIR = "sg"
+ESCAPE_URL = "http://localhost:8008/escape/sg"
 
 app = Flask("T-NOVA_Connector")
+
+
+def convert_service (nsd_file):
+  # Create converter
+  converter = TNOVAConverter(logger=app.logger, catalogue_dir=CATALOGUE_DIR)
+  # Convert the NSD given by file name
+  sg = converter.convert(nsd_file=nsd_file)
+  if sg is None:
+    app.logger.error("Service conversion was failed!")
+    return
+  # Save result NFFG into a file
+  sg_path = nsd_file.replace(NSD_DIR, SERVICE_NFFG_DIR)
+  with open(sg_path, 'w') as f:
+    f.write(sg.dump())
 
 
 @app.route("/nsd", methods=['POST'])
 def nsd ():
   try:
+    # Parse data as JSON
     data = json.loads(request.data)
     filename = data['nsd']['id']
-    dirname = os.path.realpath(os.path.dirname(__file__))
-    path = "%s/nsds/%s.json" % (dirname, filename)
+    path = "%s/%s/%s.json" % (PWD, NSD_DIR, filename)
     with open(path, 'w') as f:
-      f.write(str(data))
+      f.write(json.dumps(data))
     app.logger.info("Received NSD has been saved: %s!" % path)
-    return "OK"
+    # Initiate service conversion in a thread
+    # t = Thread(target=convert_service, name="SERVICE_CONVERTER", args=(path,))
+    # t.start()
+    convert_service(nsd_file=path)
+    # Response with 200 OK
+    return Response(status=httplib.ACCEPTED)
   except ValueError:
-    app.logger.error("Received data is not valid JSON!")
+    app.logger.exception("Received data is not valid JSON!")
     abort(500)
   except KeyError:
-    app.logger.error("Received data is not a valid NSD!")
+    app.logger.exception("Received data is not a valid NSD!")
     abort(500)
 
 
@@ -44,12 +74,11 @@ def vnfd ():
   try:
     data = json.loads(request.data)
     filename = data['id']
-    dirname = os.path.realpath(os.path.dirname(__file__))
-    path = "%s/vnf_catalogue/%s.json" % (dirname, filename)
+    path = "%s/%s/%s.json" % (PWD, CATALOGUE_DIR, filename)
     with open(path, 'w') as f:
-      f.write(str(data))
+      f.write(json.dumps(data))
     app.logger.info("Received VNFD has been saved: %s!" % path)
-    return "OK"
+    return Response(status=httplib.ACCEPTED)
   except ValueError:
     app.logger.error("Received data is not valid JSON!")
     abort(500)
@@ -58,5 +87,18 @@ def vnfd ():
     abort(500)
 
 
+@app.route("/service/<sg_id>", methods=['POST'])
+def initiate_service (sg_id):
+  app.logger.info("Received service initiation with id: %s" % sg_id)
+  sg_path = "%s/%s/%s.json" % (PWD, SERVICE_NFFG_DIR, sg_id)
+  with open(sg_path) as f:
+    sg = json.load(f)
+  ret = requests.post(url=ESCAPE_URL, json=sg)
+  app.logger.info(
+    "Service initiation has benn forwarded with result: %s" % ret.status_code)
+
+
 if __name__ == "__main__":
-  app.run(debug=True, port=5000)
+  # app.run(debug=True, port=5000)
+  logging.basicConfig(level=logging.DEBUG)
+  app.run(port=5000)
