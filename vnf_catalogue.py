33d96a6c9aa8e488b6753d56f14b9775f9c4e9ec
Janos Czentye <czentye@tmit.bme.hu>
2016-09-20 15:32:51 +0200

    Fix

--- vnf_catalogue.py
+++ vnf_catalogue.py
@@ -361,7 +361,8 @@ class VNFCatalogue(object):
       else:
         self.log.error("Got error during requesting VNFD with id: %s!" % vnf_id)
       return None
-    self.log.log(VERBOSE, "Received body:\n%s" % pprint.pformat(response.text))
+    self.log.log(VERBOSE,
+                 "Received body:\n%s" % pprint.pformat(response.json()))
     vnfd = json.loads(response.text, object_hook=self.__vnfd_object_hook)
     if self.STORE_VNFD_LOCALLY:
       self.register(id=vnf_id, vnfd=vnfd)
